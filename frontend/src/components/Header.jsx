5234453df7b431c91dc0357eeef8b2d64646a417
Malleswar-257 <sivanagamalleswar257@gmail.com>
2025-12-02 11:41:50 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/blog">Blog</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
